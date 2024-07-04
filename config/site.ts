4980cf9324f12ff276efd6b9cc76c0dd31be028a
Daria <roseannee.kim@gmail.com>
2024-07-04 18:05:11 +0200

    feat: add utils.ts, fonts.ts, site.ts

---
+++ config/site.ts
@@ -0,0 +1,15 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Template",
+  description: "Starter template.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    github: "https://github.com/roseannee",
+  },
+}
